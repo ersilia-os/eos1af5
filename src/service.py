503b5c31365bfa6d04b2e6964b88c8df50de9697
Febie Lin <133082422+febielin@users.noreply.github.com>
2023-06-29 10:27:21 -0700

    checkpoints_dir, bash update

--- src/service.py
+++ src/service.py
@@ -25,7 +25,7 @@ def load_model(framework_dir, checkpoints_dir):
 class Model(object):
     def __init__(self):
         self.DATA_FILE = "_data.csv"
-        self.PRED_FILE = "pred.csv"    // reformat
+        self.PRED_FILE = "pred.csv"  
         self.OUTPUT_FILE = "_output.csv"
         self.RUN_FILE = "_run.sh"
         self.LOG_FILE = "run.log"
@@ -53,11 +53,11 @@ class Model(object):
         run_file = os.path.join(tmp_folder, self.RUN_FILE)
         with open(run_file, "w") as f:
             lines = [
-                "python {0}/run.sh {1} {2} {3}".format(
+                "bash {0}/run.sh {1} {2}".format(
                     self.framework_dir,
                     data_file,
                     output_file,
-                    self.checkpoints_dir
+                    #self.checkpoints_dir
                 )
             ]
             f.write(os.linesep.join(lines))
